455bee099e3a83b85116c48690b7d10b24b66af1
Hugo Kaua <hugokaua2002@gmail.com>
2024-03-06 01:19:48 -0300

    att

--- cypress/e2e/DemoQA/demoQa.cy.js
+++ cypress/e2e/DemoQA/demoQa.cy.js
@@ -33,8 +33,27 @@ describe('Testes para DemoQA', ()=>{
         cy.get('td:contains("2199300512")').should('exist')
         cy.get('td:contains("02 April,2019")').should('exist')
         cy.get('td:contains("Music")').should('exist')
-
-
-
     })
+    it('Validando erro no email', ()=>{
+        cy.get('div[class="card-body"]:contains(Forms)').click()
+        cy.url().should('include', 'https://demoqa.com/forms')
+        cy.get('span:contains(Practice Form)').click()
+
+        cy.get('#firstName').as('name').click().type('Adriano')
+        cy.get('#lastName').click().type('Imperador')
+        cy.get('#userEmail').click().type('teste@gmai.comsa.rf')
+        cy.get('input[type="radio"]').first().check({force:true})
+        cy.get('#userNumber').type('2199300512')    
+        cy.get('#dateOfBirth').click()
+        cy.get('.react-datepicker__month-select').select('April')
+        cy.get('.react-datepicker__year-select').select('2019')
+        cy.get('[aria-label="Choose Tuesday, April 2nd, 2019"]').click()
+        cy.get('#hobbies-checkbox-3').first().check({force:true})
+        cy.get('#submit').click()
+
+        // validações
+        cy.get('td:contains("teste@gmai.comsa.rf")').should('exist')
+    })
+    
+
 })
